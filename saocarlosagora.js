8c12025c249c135d30287894d43f76ba77c97370
Matheus Cardoso <matheuslive@live.com>
2023-07-03 18:16:21 -0300

    rebase

--- saocarlosagora.js
+++ saocarlosagora.js
@@ -1,12 +1,17 @@
 // ==UserScript==
-// @name     _Override saocarlosemrede styles
-// @include  https://saocarlosemrede.com.br/noticias/*
+// @name     _Override saocarlosagora styles
+// @description Remove Ads from saocarlosagora
+// @include  https://www.saocarlosagora.com.br/*
 // @grant    GM_addStyle
 // @run-at   document-start
+// @version 0.0.1.20230630033015
+// @namespace https://greasyfork.org/users/1115413
 // ==/UserScript==
-
+ 
 GM_addStyle ( `
-	.td-main-page-wrap {
-  		padding-top: 48px;
-	}
+ 
+.barraazul,.billboard,.colunaBanners,.apenasMobile,.mobileNao,.squareBanner,.squareBanner336,.supperBanner,.shareAreas,.shareAreas2,.dvBanner250,.grid_4.bannerDireita {
+   display: none !important;
+}
+ 
 ` );
