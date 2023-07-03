a2e9385d975fc06fce6e134960d26161889fb84f
Matheus Cardoso <matheuslive@live.com>
2023-07-02 23:43:38 -0300

    Update CSP.user.js disable logic

--- CSP.user.js
+++ CSP.user.js
@@ -7,14 +7,13 @@ ble// ==UserScript==
 // ==/UserScript==
 
 var cspRule = localStorage.getItem("CSPBlocker");
-var cspDisable = localStorage.getItem("CSPDisable");
 
-if (!cspRule && !cspDisable) {
-	localStorage.setItem("CSPBlocker", "script-src-elem 'self'")
-	cspRule = localStorage.getItem("CSPBlocker");
+if (!cspRule) {
+	cspRule = "script-src-elem 'self'";
+	localStorage.setItem("CSPBlocker", cspRule)
 }
 
-if (cspRule && !cspDisable) {
+if (cspRule.length) {
 	const meta = document.createElement("meta");
 	meta.setAttribute("http-equiv", "Content-Security-Policy");
 	meta.setAttribute("content", cspRule);
@@ -30,19 +29,11 @@ if (cspRule && !cspDisable) {
 GM_registerMenuCommand("No JavaScript", () => { localStorage.setItem("CSPBlocker", "script-src 'none'") });
 GM_registerMenuCommand("No Third-Party", () => { localStorage.setItem("CSPBlocker", "default-src 'unsafe-inline' 'self'") });
 GM_registerMenuCommand("Restrict js-elem", () => { localStorage.setItem("CSPBlocker", "script-src-elem 'self'") });
-
 GM_registerMenuCommand("Edit CSP rules", () => {
 	const newrule = prompt("Editing CSP rules", localStorage.getItem("CSPBlocker") || "");
 	if (newrule && newrule != "") {
 		localStorage.setItem("CSPBlocker", newrule)
 	}
 });
-
-if (cspDisable) {
-	GM_registerMenuCommand("CSP Enable", () => { localStorage.removeItem("CSPDisable") });
-} else {
-	GM_registerMenuCommand("CSP Disable", () => { localStorage.setItem("CSPDisable") });
-}
-
-GM_registerMenuCommand("Clear CSP rules", () => { localStorage.removeItem("CSPBlocker") });
+GM_registerMenuCommand("Clear CSP rules", () => { localStorage.setItem("CSPBlocker", "") });
 
