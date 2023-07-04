61496d480780f61918a787c517a93b3417025654
Matheus Cardoso <matheuslive@live.com>
2023-07-04 13:59:04 -0300

    include google-analytics.js

---
+++ google-analytics.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         Block Google ads & analytics
+// @namespace    https://greasyfork.org/en/users/1115413-karrdozo
+// @version      1.1.5
+// @license      MIT
+// @description  Block annoying Google ads & analytics
+// @author       karrdozo
+// @include      http*://**
+// @run-at       document-start
+// ==/UserScript==
+
+(() => {
+  // 1. intercept ads trigger
+  const script = document.createElement('script');
+  script.innerHTML = `
+    interceptProp(window, 'adsbygoogle', {push: () => {}});
+    interceptProp(window, 'dataLayer', {push: () => {}});
+    interceptProp(window, 'ga', {push: () => {}});
+    interceptProp(window, 'googletag', {push: () => {}});
+    interceptProp(window, 'google_tag_manager', {push: () => {}});
+    interceptProp(window, 'google_tag_data', {push: () => {}});
+
+    function interceptProp(obj, prop, overrideVal) {
+      let _val;
+      Object.defineProperty(obj, prop, {
+        get() {return _val;},
+        set(val) {
+          _val = val ;
+          Object.assign(_val, overrideVal)
+        }
+      })
+    }
+  `;
+  document.head.prepend(script);
+
+  // 2. remove exist ads
+  const style = document.createElement('style');
+  style.innerHTML = '* [data-ad-client] {display: none !important}';
+  document.head.append(style);
+
+  // 3. disable ads scripts add
+  new MutationObserver((mutations) => {
+    mutations.forEach(item => {
+      if (item.type === 'childList') {
+        item.addedNodes.forEach(node => {
+          if (
+            node.src &&
+            node.src.includes('ads')
+          ) {
+            node.remove();
+          }
+        });
+      }
+    });
+  }).observe(document.head, {childList: true});
+})();
