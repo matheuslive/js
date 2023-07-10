f7830b1f5cb262a440b1d8ef5bc4126ee944c0bd
Matheus Cardoso <matheuslive@live.com>
2023-07-09 22:31:04 -0300

    update windy

--- windy.user.js
+++ windy.user.js
@@ -9,9 +9,11 @@
 // @grant        none
 // ==/UserScript==
 
+console.log('Starting script..')
 
 if (W.rootScope.community === 'https://community.windy.com') {
+	console.log('its windy!!')
 	W.Switch.getEl("satellite").click();
 }
 
-
+console.log('end script..')
